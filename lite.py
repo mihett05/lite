4ba49b62901d8aaffbe92b4fac03a3cf54061d1d
mihett05 <mihett05@yandex.ru>
2019-07-31 11:22:15 +0500

    Added request dict

--- lite.py
+++ lite.py
@@ -11,8 +11,18 @@ class Lite:
                 route["methods"] = ["get"]
             route["methods"] = frozenset(route["methods"])
 
-            async def undefined_function(request):
-                response = route["handler"](request)
+            async def function_wrapper(request):
+                req = {
+                    "original": request,
+                    "params": dict(request.match_info).values(),
+                    "query": dict(request.rel_url.query),
+                    "method": request.method,
+                    "url": request.url,
+                    "headers": dict(request.headers),
+                    "cookies": dict(request.cookies),
+                }
+                
+                response = route["handler"](req, *req["params"])
                 return {
                     dict: web.Response(text=json.dumps(response), content_type="application/json"),
                     str: web.Response(text=str(response), content_type="text/html")
@@ -20,11 +30,11 @@ class Lite:
 
             for method in route["methods"]:
                 {
-                    "get": lambda: self.app.router.add_get(route["path"], undefined_function),
-                    "post": lambda: self.app.router.add_post(route["path"], undefined_function),
-                    "put": lambda: self.app.router.add_put(route["path"], undefined_function),
-                    "delete": lambda: self.app.router.add_delete(route["path"], undefined_function)
-                }.setdefault(method.lower(), lambda: self.app.router.add_options(route["path"], undefined_function))()
+                    "get": lambda: self.app.router.add_get(route["path"], function_wrapper),
+                    "post": lambda: self.app.router.add_post(route["path"], function_wrapper),
+                    "put": lambda: self.app.router.add_put(route["path"], function_wrapper),
+                    "delete": lambda: self.app.router.add_delete(route["path"], function_wrapper)
+                }.setdefault(method.lower(), lambda: self.app.router.add_options(route["path"], function_wrapper))()
         web.run_app(self.app)
 
 
