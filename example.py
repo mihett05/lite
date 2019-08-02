a8fc122a96938b88b46f57c306b30c733ed73571
mihett05 <mihett05@yandex.ru>
2019-08-02 16:42:59 +0500

    Update example.py

--- example.py
+++ example.py
@@ -5,18 +5,10 @@ def hello(request):
     return {"hello": "world"}
 
 
-def param(request, prm):
-    return {"prm": prm}
-
-
 Lite([
     {
         "path": "/",
         "handler": hello
-    },
-    {
-        "path": "/param/{str:prm}",
-        "handler": param
     }
 ]).run()
 
