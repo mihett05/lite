a2326790a4ab3ba125db72ddecc751efeef95909
mihett05 <mihett05@yandex.ru>
2019-08-02 20:42:50 +0500

    Update example.py

--- example.py
+++ example.py
@@ -5,10 +5,22 @@ def hello(request):
     return {"hello": "world"}
 
 
+def func_with_arg(request, arg):
+    return {"arg": arg}
+
+
 Lite([
     {
         "path": "/",
         "handler": hello
+    },
+    {
+        "path": "/arg/{str:arg}",
+        "handler": func_with_arg
+    },
+    {
+        "path": "404",
+        "handler": lambda req: "Page Not Found :("
     }
 ]).run()
 
